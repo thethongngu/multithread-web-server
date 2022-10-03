ebfdd5c22990c74b8a46eec1fcccacdc7889c244
thethongngu <nguyenthethong1996@gmail.com>
2022-10-03 20:50:15 +0800

    initial commit: single thread web server

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+pub struct ThreadPool;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let mut threads = Vec::with_capacity(size);
+        for _ in 0..size {}
+
+        ThreadPool { threads }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+    }
+}
